7ab686d45ac685b6fc77cd76b7ce2587020a6d47
loiccheck404 <ndzieloic334@gmail.com>
2026-04-05 13:35:01 +0100

    Implement DetailsPage component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from "./components/RegisterPage/RegisterPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
 function App() {
   const navigate = useNavigate();
   return (
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
         <Route path="/app" element={<MainPage />} />
         <Route path="/app/login" element={<LoginPage />} />
         <Route path="/app/register" element={<RegisterPage />} />
